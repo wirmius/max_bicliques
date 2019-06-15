0acfdebb1e7366c0c465eeec18b71d8e55731368
Heridaspollux <34633555+Heridaspollux@users.noreply.github.com>
2019-06-15 22:19:07 +0200

    Update Biclique.java

--- src/graphs/Biclique.java
+++ src/graphs/Biclique.java
@@ -7,10 +7,43 @@ import java.util.Set;
  *
  */
 public class Biclique {
-    private Graph graph;
-    private Set<Graph.Vertex> left;
-    private Set<Graph.Vertex> right;
+    boolean isMaximal;
+	private List<Vertex> leftNodes;
+	private List<Vertex> rightNodes;
+    Biclique(List<Vertex> leftV, List<Vertex> rightV)
+    {
+        leftNodes = leftV;
+        rightNodes = rightV;
+      
+        for(int i=0;i<leftNodes.size();i++)
+        {
 
+            for(int j=0;j<rightNodes.size();j++)
+            {
+                Vertex left = leftNodes.get(i);
+                Vertex right = rightNodes.get(j);
+                try
+                {
+                    Vertex.addEdge(left,right);
+                }
+                catch (RuntimeException e) {}
+            }
+        }
 
+      }
+    
+    public List<Vertex> getLeftNodes() {
+        return leftNodes;
+    }
 
+    public List<Vertex> getRightNodes() {
+        return rightNodes;
+    }
+
+    public boolean isMaximal() {
+        return isMaximal;
+    }
+
+    
 }
+
