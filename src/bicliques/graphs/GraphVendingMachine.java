bbf0ec052a47704a6a597488aa6ebfc7b7e1020a
Roland Koppenberger <roland@koppenberger.com>
2019-06-24 17:34:09 +0200

    GraphVendingMachine - set lemmeHaveAnEmptyGraph to public.

--- src/bicliques/graphs/GraphVendingMachine.java
+++ src/bicliques/graphs/GraphVendingMachine.java
@@ -1,7 +1,7 @@
 package bicliques.graphs;
 
 public class GraphVendingMachine {
-    static <V extends Comparable<? super V>, E extends Comparable<? super E>> Graph<V, E> lemmeHaveAnEmptyGraph(boolean directed) {
+    public static <V extends Comparable<? super V>, E extends Comparable<? super E>> Graph<V, E> lemmeHaveAnEmptyGraph(boolean directed) {
         return new AdjacencyMapGraph<V, E>(directed);
     }
 }
