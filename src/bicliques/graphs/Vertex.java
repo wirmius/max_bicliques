740cc215fd4c22cfa6b3c2231894e5f602a40752
Heridaspollux <34633555+Heridaspollux@users.noreply.github.com>
2019-06-26 21:50:48 +0200

    Add files via upload

---
+++ src/bicliques/graphs/Vertex.java
@@ -0,0 +1,110 @@
+package bicliques.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex{
+    private int label;
+    private List<Vertex> neighbours  = new ArrayList<>();
+
+    Vertex(){}
+    Vertex(int label)
+    {
+        this.label = label;
+    }
+
+    List<Vertex> getNeighbours()
+    {
+        return neighbours;
+    }
+
+    public int getLabel()
+    {
+        return label;
+    }
+
+    private void addNeighbour(Vertex v)
+    {
+        if(neighbours.contains(v))
+            throw new RuntimeException("Vertex::add_neighbour - vertex is already a neighbour.");
+        neighbours.add(v);
+    }
+
+    public void removeNeighbour(Vertex v)
+    {
+        if(neighbours.contains(v))
+            neighbours.remove(v);
+
+    }
+
+    static void addEdge(Vertex v1, Vertex v2)
+    {
+        try
+        {
+            v1.addNeighbour(v2);
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Vertex::add_edge - " + Integer.toString(v2.label) + " is already a neighbour of " + Integer.toString(v1.label));
+        }
+        try
+        {
+            v2.addNeighbour(v1);
+        }
+        catch (RuntimeException e) {
+            throw new RuntimeException("Vertex::add_edge - " + Integer.toString(v1.label) + " is already a neighbour of " + Integer.toString(v2.label));
+        }
+    }
+
+    void removeEdge(Vertex v1, Vertex v2)
+    {
+            v1.removeNeighbour(v2);
+            v2.removeNeighbour(v1);
+    }
+
+
+
+    int getNeighboursSize()
+    {
+        return neighbours.size();
+    }
+
+    public int numberOfNeighboursOfVInSet(List<Vertex> set)
+    {
+        int out = 0;
+        for (Vertex aSet : set)
+        {
+            if (isNeighbour(aSet))
+            {
+                out += 1;
+            }
+        }
+        return out;
+    }
+
+    public boolean isNeighbour(Vertex otherV)
+    {
+        return neighbours.contains(otherV);
+    }
+
+    public boolean isEqual(Vertex otherV)
+    {
+        return this.label == otherV.label;
+    }
+
+    boolean isMember(List<Vertex> set)
+    {
+        boolean out = false;
+        for(Vertex v:set)
+        {
+            if(this.equals(v))
+                out = true;
+        }
+        return out;
+    }
+
+    public int compareTo(Vertex other)
+    {
+        return this.getNeighboursSize() - other.getNeighboursSize();
+    }
+
+}
