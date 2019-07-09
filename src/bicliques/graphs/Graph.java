3e03a558550c3846d72a76a92d2ace26850b72b2
Roland Koppenberger <roland@koppenberger.com>
2019-07-09 16:08:53 +0200

    interface Graph: return type of getNeighbours() changed.

--- src/bicliques/graphs/Graph.java
+++ src/bicliques/graphs/Graph.java
@@ -12,7 +12,7 @@ public interface Graph<V extends Comparable<? super V>, E extends Comparable<? s
         void setElem(V elem);
         Set<? extends Edge> getOutgoingEdges();
         Set<? extends Edge> getIncomingEdges();
-        Set<? extends Vertex<V>> getNeighbours();
+        Set<Vertex<V>> getNeighbours();
     }
 
     interface Edge<E> {
